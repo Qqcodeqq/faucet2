22086ed99be3899af432cfda92063a30fedc83c0
Qqcodeqq <119717141+Qqcodeqq@users.noreply.github.com>
2023-04-07 20:34:34 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const faucetABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"addr","type":"address"}],"name":"AddressRemoved","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"addr","type":"address"}],"name":"AddressWhitelisted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"claimer","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"EtherClaimed","type":"event"},{"inputs":[],"name":"addFunds","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_addr","type":"address"}],"name":"addToWhitelist","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address[]","name":"_addresses","type":"address[]"}],"name":"addToWhitelistBatch","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"claim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_addr","type":"address"}],"name":"isWhitelisted","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_addr","type":"address"}],"name":"removeFromWhitelist","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+const faucetAddress = "0x86a845ea1F6AAe91e3A2Fa2c4cA971DC5f824ED2"; // Replace with your contract's address
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+let accounts;
+
+async function loadAccounts() {
+  accounts = await web3.eth.getAccounts();
+}
+
+async function getBalance() {
+  const faucetContract = new web3.eth.Contract(faucetABI, faucetAddress);
+  const balance = await faucetContract.methods.getBalance().call();
+  document.getElementById("balance").innerText = `Balance: ${web3.utils.fromWei(balance, "ether")} ETH`;
+}
+
+async function claim() {
+  const faucetContract = new web3.eth.Contract(faucetABI, faucetAddress);
+  try {
+    await faucetContract.methods.claim().send({ from: accounts[0] });
+    document.getElementById("claimStatus").innerText = "Claim successful!";
+  } catch (err) {
+    document.getElementById("claimStatus").innerText = "Claim failed!";
+  }
+}
+
+document.getElementById("getBalance").addEventListener("click", getBalance);
+document.getElementById("claim").addEventListener("click", claim);
+window.addEventListener("load", loadAccounts);
